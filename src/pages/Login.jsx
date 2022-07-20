4cd07c5f855c9dfdd579a8c4123f3b9a05a428f1
Jay Bhogayata <jaybhogayata53@gmail.com>
2022-07-20 19:33:03 +0530

    minor css changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import loginimg from "../assets/loginimg.svg";
-import { FaGithub , FaGoogle } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div className="flex  h-screen lg:flex-row  flex-col w-full ">
-      <div className="lg:w-1/2  flex w-full h-1/3 lg:h-screen flex-col justify-around space-y-10 ">
+      <div className="lg:w-1/2  lg:mt-0 mt-2 flex w-full h-1/3 lg:h-screen flex-col justify-around space-y-10 ">
         <h1 className="text-5xl  font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-800 via-violet-900 to-purple-800 mx-auto lg:mt-6 mb-3 p-2">
-          CodeXAcdamy
+          CodeXAcademy
         </h1>
         <img
           src={loginimg}
           alt=""
-          className="lg:w-2/3 w-1/2 h-1/2 mx-auto lg:mt-0 mt-2"
+          className="lg:w-2/3 w-1/2 h-1/2 mx-auto lg:mt-0 "
         />
       </div>
       <div className="lg:w-1/2 md:w-2/3 mx-auto flex flex-col w-full h-1/2 lg:h-screen  lg:mt-0 mt-8 space-y-10 ">
@@ -41,8 +41,13 @@ const Login = () => {
           <button className="lg:w-2/3 w-full bg-purple-600 p-2 rounded-lg text-xl ">
             Sign up
           </button>
-          <div  className="lg:mx-0   mx-auto">        
-          <p>Alrady have an account ? <a href="{#}" className="ml-3 text-blue-700 underline">Login</a></p>
+          <div className="lg:mx-0   mx-auto">
+            <p>
+              Alrady have an account ?{" "}
+              <a href="{#}" className="ml-3 text-blue-700 underline">
+                Login
+              </a>
+            </p>
           </div>
           <p className="lg:w-2/3 w-full  p-2 rounded-lg text-xl flex">
             <span className="mx-auto">or</span>
@@ -53,16 +58,15 @@ const Login = () => {
             "
             >
               <FaGoogle className="text-white my-auto" />
-              <p className= ""> Sign up with Google</p>
+              <p className=""> Sign up with Google</p>
             </button>
             <button
               className="lg:w-2/3 w-full bg-stone-800  border-[2px] border-gray-50 p-2 rounded-lg text-xl flex justify-around
             "
             >
               <FaGithub className="text-white my-auto" />
-              <p className= ""> Sign up with Github</p>
+              <p className=""> Sign up with Github</p>
             </button>
-            
           </div>
         </div>
       </div>
