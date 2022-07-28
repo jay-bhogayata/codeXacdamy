16d3fde5048c0aa62b45354332d411347ab420c0
Jay Bhogayata <jaybhogayata53@gmail.com>
2022-07-28 19:22:32 +0530

    landing page is ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Laern from "../assets/Learn.svg";
 import google from "../assets/google.svg";
 import aws from "../assets/aws.svg";
 import microsoft from "../assets/microsoft.svg";
 import zoho from "../assets/zoho.svg";
 import zomato from "../assets/zomato.svg";
+import think from "../assets/think.svg";
 
 import { BsFillLaptopFill } from "react-icons/bs";
 
@@ -29,15 +31,17 @@ const Home = () => {
             Start Learnning Today
           </button>
         </div>
-        <div className="lg:w-1/2 lg:h-1/2 w-full flex flex-col justify-center p-5 md:p-20 lg:px-36">
-          <img src={Laern} alt="" />
+        <div className="lg:w-1/2 lg:h-1/2 w-full  flex  justify-center p-5 md:p-20 lg:px-36">
+          <img src={Laern} alt="" className="lg:w-full " />
         </div>
       </div>
 
       {/* section 2 */}
       <div className="bg-[#f1f1f1]   flex lg:flex-row flex-col text-black p-3">
-        <div className="w-full   lg:mx-32 
-        rounded-3xl mb-10 flex lg:flex-row flex-col lg:space-y-0 space-y-1 ">
+        <div
+          className="w-full   lg:mx-32 
+        rounded-3xl mb-10 flex lg:flex-row flex-col lg:space-y-0 space-y-1 "
+        >
           <p className="text-4xl font-bold text-[#1b1b1b] lg:w-1/3 flex my-auto p-5 leading-10 mt-5">
             Our numbers tells more about us
           </p>
@@ -70,7 +74,8 @@ const Home = () => {
               We add value to our student's journey
             </p>
             <p className="text-md font-semibold text-[#1b1b1b] p-4 lg:w-2/3 ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quod eum culpa dicta laboriosam. Adipisci 
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Distinctio quod eum culpa dicta laboriosam. Adipisci
             </p>
             <button className="bg-[#1b1b1b] text-lg text-gray-300 p-3 rounded-xl mt-8 ml-3">
               Learn more
@@ -117,62 +122,109 @@ const Home = () => {
                 Online classes from the best coders
               </p>
             </div>
-            
           </div>
         </div>
       </div>
 
       {/* section 4 */}
       <div className="bg-[#f1f1f1] flex lg:flex-row flex-col text-black p-3">
-        <div className="w-full  p-4 lg:mx-32 
-        rounded-3xl mb-10 flex  flex-col lg:space-y-0 space-y-3 ">
-          <p className="text-3xl font-bold text-[#1b1b1b]/90 mb-10">We'll help you learn what you like</p>
+        <div
+          className="w-full  p-4 lg:mx-32 
+        rounded-3xl mb-10 flex  flex-col lg:space-y-0 space-y-3 "
+        >
+          <p className="text-3xl font-bold text-[#1b1b1b]/90 mb-10">
+            We'll help you learn what you like
+          </p>
           <div className=" flex   lg:space-x-10 lg:flex-row flex-col lg:space-y-0 space-y-8">
-          <div className=" lg:w-2/5 space-y-2 ">
-            <span className="text-4xl font-bold text-blue-400">01</span>
-            <p className="text-2xl font-bold text-[#1b1b1b]/90">Learn at your own pace</p>
-            <p className="text-md font-semibold text-gray-600/95">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quod eum culpa dicta laboriosam. Adipisci 
-            </p>
-          </div>
-          <div className="lg:w-2/5 space-y-2 ">
-            <span className="text-4xl font-bold text-blue-400">02</span>
-            <p className="text-2xl font-bold text-[#1b1b1b]/90">Learn from industry experts</p>
-            <p className="text-md font-semibold text-gray-600/95">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quod eum culpa dicta laboriosam. Adipisci 
-            </p>
-          </div>
-         
-          <div className="lg:w-2/5 space-y-2 ">
-            <span className="text-4xl font-bold text-blue-400">03</span>
-            <p className="text-2xl font-bold text-[#1b1b1b]/90">Large course library</p>
-            <p className="text-md font-semibold text-gray-600/95">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio quod eum culpa dicta laboriosam. Adipisci 
-            </p>
-          </div>
-         
+            <div className=" lg:w-2/5 space-y-2 ">
+              <span className="text-4xl font-bold text-blue-400">01</span>
+              <p className="text-2xl font-bold text-[#1b1b1b]/90">
+                Learn at your own pace
+              </p>
+              <p className="text-md font-semibold text-gray-600/95">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Distinctio quod eum culpa dicta laboriosam. Adipisci
+              </p>
+            </div>
+            <div className="lg:w-2/5 space-y-2 ">
+              <span className="text-4xl font-bold text-blue-400">02</span>
+              <p className="text-2xl font-bold text-[#1b1b1b]/90">
+                Learn from industry experts
+              </p>
+              <p className="text-md font-semibold text-gray-600/95">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Distinctio quod eum culpa dicta laboriosam. Adipisci
+              </p>
+            </div>
+
+            <div className="lg:w-2/5 space-y-2 ">
+              <span className="text-4xl font-bold text-blue-400">03</span>
+              <p className="text-2xl font-bold text-[#1b1b1b]/90">
+                Large course library
+              </p>
+              <p className="text-md font-semibold text-gray-600/95">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Distinctio quod eum culpa dicta laboriosam. Adipisci
+              </p>
+            </div>
           </div>
         </div>
       </div>
 
       {/* section 5 */}
       <div className="bg-[#f1f1f1] flex flex-col text-black p-3">
-        
-        <p className="lg:mx-36 text-3xl text-[#1b1b1b]/90 font-bold">Our student's work at</p>
+        <p className="lg:mx-36 p-3 text-3xl text-[#1b1b1b]/90 font-bold">
+          Our student's work at
+        </p>
         <div className="flex lg:flex-row flex-wrap lg:mx-32 lg:mt-14   lg:space-y-0 mt-10 ">
-        <img src={google} alt="" className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"/>
-        <img src={microsoft} alt="" className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"/>
-        
-        <img src={zomato} alt="" className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"/>
-        <img src={zoho} alt="" className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"/>
+          <img
+            src={google}
+            alt=""
+            className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"
+          />
+          <img
+            src={microsoft}
+            alt=""
+            className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"
+          />
+
+          <img
+            src={zomato}
+            alt=""
+            className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"
+          />
+          <img
+            src={zoho}
+            alt=""
+            className="lg:w-1/4 w-2/5 mx-auto lg:px-10 p-2"
+          />
         </div>
       </div>
 
       {/* section 6 */}
-      <div className="bg-[#f1f1f1] flex flex-col text-black p-3">
-
+      <div className="bg-[#f1f1f1]  flex lg:flex-row flex-col text-black p-3">
+        <div className="lg:mx-36 flex lg:flex-row flex-col w-full mt-10">
+          <div className="p-2 lg:w-1/2 mx-auto my-auto">
+            <img src={think} alt="" className="p-3 lg:w-2/3" />
+          </div>
+          <div className=" p-2 lg:w-1/2">
+          <p className="p-3 my-2 text-3xl  text-[#1b1b1b]/90 font-bold">
+              {" "}
+              Don't think start Learnning tody
+            </p>
+            <p className="p-3 text-xl font-semibold text-gray-600/95 ">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              assumenda reprehenderit perspiciatis rem iusto odit.
+            </p>
+            <button className="bg-[#1b1b1b] text-lg text-gray-300 p-3 rounded-xl mt-8 lg:w-1/3 lg:mx-3 ml-3 ">
+              Join Now
+            </button>
+          </div>
+        </div>
       </div>
 
+      <Footer/>
     </div>
   );
 };
